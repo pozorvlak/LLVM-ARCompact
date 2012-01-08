66b842d8419be5e65ded6d1bfe334a0bf08e05eb
Stephen McGruer <stephen_mcgruer@gmail.com>
2012-01-08 17:34:51 +0000

    Added test for NOT

---
+++ test/not_test/test.c
@@ -0,0 +1,16 @@
+int f(int x) {
+  // Register version.
+  int a = ~x;
+
+  // Unsigned-immediate version.
+  int b = ~4;
+
+  // Long-immediate version.
+  int c = ~6000;
+
+  return 0;
+}
+
+int main() {
+  return 0;
+}
