fc5ba9f8ddd49ff3eaf14e0a1718953ee374c322
Stephen McGruer <stephen_mcgruer@gmail.com>
2012-07-22 03:12:15 +0100

    Slight change to arclibs script.

--- arclibs/compile.sh
+++ arclibs/compile.sh
@@ -1,6 +1,8 @@
 rm -rf obj
 mkdir obj
 
+rm libgcc_extra.a
+
 /afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-gcc -c src/ledf2.c -o obj/ledf2.o
 /afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-gcc -c src/unorddf2.c -o obj/unorddf2.o
 
