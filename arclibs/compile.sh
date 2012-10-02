a47dad6afe46f32e5a3b9bff1cb7703ba1ac9433
Stephen McGruer <stephen_mcgruer@gmail.com>
2012-10-02 16:58:59 +0100

    Added __eqdf2/__nedf2 to libgcc_extra

--- arclibs/compile.sh
+++ arclibs/compile.sh
@@ -5,5 +5,6 @@ rm libgcc_extra.a
 
 /afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-gcc -c src/ledf2.c -o obj/ledf2.o
 /afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-gcc -c src/unorddf2.c -o obj/unorddf2.o
+/afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-gcc -c src/eqdf2.c -o obj/eqdf2.o
 
-/afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-ar rcs libgcc_extra.a obj/ledf2.o obj/unorddf2.o
+/afs/inf.ed.ac.uk/group/project/arc_tools/arc_gcc_2.3/arc-elf32/bin/arc-elf32-ar rcs libgcc_extra.a obj/ledf2.o obj/unorddf2.o obj/eqdf2.o
